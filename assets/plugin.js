3acb7a7a8f1485c4997481deb4ecad286f56dd87
Samchon <sam@myrealplan.co.kr>
2019-09-17 11:39:19 +0900

    book-body & book-inner

--- assets/plugin.js
+++ assets/plugin.js
@@ -3,6 +3,9 @@ require(["gitbook"], function (gitbook)
     gitbook.events.bind("page.change", function() 
     {
         if (window.location.href.indexOf("#") === -1)
+        {
+            document.getElementsByClassName("book-body")[0].scrollTo(0, 0);
             document.getElementsByClassName("body-inner")[0].scrollTo(0, 0);
+        }
     });
 });
